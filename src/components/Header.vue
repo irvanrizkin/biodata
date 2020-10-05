9a093781b34278969e51c18287cd64f937be3618
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2020-10-05 14:39:44 +0700

    add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="bg-orange-400 mx-auto text-center">
+        <a href="#">
+            <div class="text-2xl font-bold py-4 text-white">Biodata</div>
+        </a>
+    </div>
+</template>
