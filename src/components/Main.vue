44be85a04035c1755103180cf92c91bbd7bb5f25
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2020-10-05 14:40:42 +0700

    add main component

---
+++ src/components/Main.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="mx-auto">
+        <div class="m-6 lg:m-8 grid grid-cols-1 gap-8 md:grid-cols-2">
+            <div class="">
+                <img src="Foto.jpg" alt="" class="w-full my-auto">
+            </div>
+            <div class="my-auto">
+                <div class="text-4xl font-bold">Irvan Rizki Nugraha</div>
+                <div class="text-2xl mb-4">Teknologi Informasi</div>
+                <table class="table-fixed">
+                    <tr class="">
+                        <td class="font-bold w-1/4">NIM</td>
+                        <td>195150701111015</td>
+                    </tr>
+                    <tr class>
+                        <td class="font-bold">Hobby</td>
+                        <td>
+                            <div class="inline-block mr-2 mt-2 rounded-full py-2 px-4 bg-orange-400 font-medium transition duration-500 ease-in-out hover:bg-yellow-400 transform hover:-translate-y-1 hover:scale-110">Renang</div>
+                            <div class="inline-block mr-2 mt-2 rounded-full py-2 px-4 bg-orange-400 font-medium transition duration-500 ease-in-out hover:bg-yellow-400 transform hover:-translate-y-1 hover:scale-110">Coding</div>
+                            <div class="inline-block mr-2 mt-2 rounded-full py-2 px-4 bg-orange-400 font-medium transition duration-500 ease-in-out hover:bg-yellow-400 transform hover:-translate-y-1 hover:scale-110">Bersepeda</div>
+                        </td>
+                    </tr>
+                    <tr class>
+                        <td class="font-bold">Kelebihan</td>
+                        <td>Suka mencoba hal yang baru</td>
+                    </tr>
+                    <tr></tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
